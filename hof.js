375c9efe75628c30d18a28de746019f9b0676c3f
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-10 10:04:02 -0400

    js checkpoint

--- hof.js
+++ hof.js
@@ -1,18 +1,42 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations
 
 var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
+  {name: 'Jane', age: 32},
+  {name: 'Jessica', age: 34},
+  {name: 'Jasmine', age: 39}
 ]
 
 // Use the `map` array method to create a new array containing the ages of each
 // person in the `people` array. Assign the returned array to a variable
 // called `peoplesAges`.
 
+var getAges = function () {
+  var person = people[i]
+  return person.age
+}
 
+function map (people, getAges) {
+  var peoplesAges = []
+  for (i = 0; i < people.length; i++) {
+    peoplesAges.push(getAges(person))
+    return peoplesAges
+  }
+}
 
-
+people.map(getAges)
 // Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are older than 35. Assign the returned array
 // to a variable called `peopleOlderThan35`.
+
+var filterAge = function () {
+  return people.age > 35
+}
+
+function filter (people, filterAge) {
+  var olderThan35 = []
+  for (i = 0; i < people.length; i++) {
+    if (filterAge(people[i])) {
+      olderThan35.push(people[i])
+    }
+  }
+}
--- oojs.js
+++ oojs.js
@@ -5,16 +5,24 @@
 // - a `songs` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addSong` method that adds a song (string) to the `songs` array
 
-
+class Playlist {
+  constructor (albumTitle) {
+    this.title = albumTitle
+    songs = []
+  };
+  addSong (song) {
+    songs.push(song)
+  }
+}
 
 // Create an instance of the Playlist class and set it to a variable called `myPlaylist`
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 
-
-
+var myPlaylist = new Playlist('Michael Jackson')
+myPlaylist.addSong('Thriller')
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
+if (typeof Playlist !== 'undefined') {
   module.exports = {
     Playlist
   }
